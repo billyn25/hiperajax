972e969e3616c38f3ff855f046664d94bfbd919a
billyn25 <billyndavid@gmail.com>
2026-07-24 09:00:03 +0200

    Update probar-mongodb.js

--- functions/probar-mongodb.js
+++ functions/probar-mongodb.js
@@ -1,1 +1,13 @@
 
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ok: true,
+      mensaje: "La función de Netlify funciona correctamente"
+    })
+  };
+};
